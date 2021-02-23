c10e4aeaed6eb91c9e7b34fa48d6a7f5eccbc1bf
RaiN <szabogya81@gmail.com>
2021-02-23 13:24:30 +0100

    Add type to Product data, type enum, remove some props from Product

---
+++ src/app/model/product-type.enum.ts
@@ -0,0 +1,9 @@
+export enum ProductType {
+  Action = "action",
+  Horror = "horror",
+  Adventure = "adventure",
+  Drama = "drama",
+  Comedy = "comedy",
+  Kid = "kid",
+  Other = "other"
+}
--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -1,13 +1,12 @@
 export class Product {
   id: number = 0;
   name: string = '';
-  //type: string = '';
   catId: number = 0;
+  type: string = '';
   description: string = '';
   price: number = 0;
   featured: boolean = false;
   active: boolean = false;
 
   imagePath: string = '';
-  stock: number = 0;
 }
