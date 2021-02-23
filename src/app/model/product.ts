16245652613ae45500f0246a870c198d5e0edb5c
RaiN <szabogya81@gmail.com>
2021-02-23 08:59:23 +0100

    Product, Category classes

---
+++ src/app/model/product.ts
@@ -0,0 +1,14 @@
+export class Product {
+  id: number = 0;
+  name: string = '';
+  type: string = '';
+  catId: number = 0;
+  description: string = '';
+  price: number = 0;
+  featured: boolean = false;
+  active: boolean = false;
+
+  imagePath: string = '';
+  stock: number = 0;
+  salePrice?: number = undefined;
+}
