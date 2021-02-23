1af5ede2a0ad6d1e66422a8a2d0f39a126a695f8
ethuringer <ethuringer@gmail.com>
2021-02-23 12:54:55 +0100

    modify order class

--- src/app/model/order.ts
+++ src/app/model/order.ts
@@ -1,9 +1,10 @@
+import { Product } from "./product";
 import { Status } from "./status.enum";
 
 export class Order {
     id: number = 0;
     customerID: number = 0;
-    productID: number = 0;
+    products: Product[] = [];
     amount: number = 0;
     status: Status = Status.New;
 }
