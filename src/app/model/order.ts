08ce67c5384de14e5148748db1ddb98824296800
ethuringer <ethuringer@gmail.com>
2021-02-23 15:41:14 +0100

    change productID

--- src/app/model/order.ts
+++ src/app/model/order.ts
@@ -1,10 +1,9 @@
-import { Product } from "./product";
 import { Status } from "./status.enum";
 
 export class Order {
     id: number = 0;
     customerID: number = 0;
-    products: Product[] = [];
+    productID: number[] = [];
     amount: number = 0;
     status: Status = Status.New;
 }
