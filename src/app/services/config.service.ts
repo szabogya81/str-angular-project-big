2cfd9fe12fc014e6cb433bbc931e6e0c73278859
RaiN <szabogya81@gmail.com>
2021-02-26 18:13:46 +0100

    Config service, ngBoostrap, TypeAhead

---
+++ src/app/services/config.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+export interface ITableCol {
+  key: string;
+  title: string;
+
+  icon?: string;
+  pattern?: string;
+  visible?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ConfigService {
+
+  constructor() { }
+
+  categoryTableColumns: ITableCol[] = [
+    { key: 'id', title: 'ID', visible: true },
+    { key: 'name', title: 'Name', visible: true },
+    { key: 'description', title: 'Description',  visible: true }
+  ];
+
+  static baseUrl: string = 'http://localhost:3000/';
+
+  static categoriesUrl: string = `${ConfigService.baseUrl}categories`;
+  static productsUrl: string = `${ConfigService.baseUrl}products`;
+}
