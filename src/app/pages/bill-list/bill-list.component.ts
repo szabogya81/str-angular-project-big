591e1625237f183d68975fe2c4b94e4a6d809469
B.Viktor <benkovik@gmail.com>
2021-03-02 00:05:03 +0100

    column sorter inserted, design updated

--- src/app/pages/bill-list/bill-list.component.ts
+++ src/app/pages/bill-list/bill-list.component.ts
@@ -4,6 +4,14 @@ import { Bill } from 'src/app/model/bill';
 import { Status } from 'src/app/model/status.enum';
 import { BillService } from 'src/app/services/bill.service';
 
+
+export enum Order {
+  asc,
+  desc,
+  none
+}
+
+
 @Component({
   selector: 'app-bill-list',
   templateUrl: './bill-list.component.html',
@@ -18,10 +26,43 @@ export class BillListComponent implements OnInit {
   filterKey: string = 'id';
   filterKeys: string[] = Object.keys(new Bill());
 
+  // Sorter
+  columnKey: string = '';
+  direction: string = '';
+
+
   constructor(private billService: BillService) { 
   }
 
   ngOnInit(): void {
   }
 
+  // Sorter
+  onColumnSelect(key: string): void {
+    // first click on the column
+    if (this.columnKey != key) {
+      this.direction = 'asc';
+    } else {
+      // 2nd, 3rd, etc. click on the same column.
+      // list order should by switched.
+      this.direction = this.swichDirectionValue();
+    }
+    this.columnKey = key;
+  }
+
+  swichDirectionValue(): any {
+    switch (this.direction) {
+      case 'asc':
+        return this.direction = 'dsc';
+      case 'dsc':
+        return this.direction = 'asc';
+      default:
+        return this.direction = 'asc';
+    }
+  }
+
+
+  getId(): void {
+    console.log("Debug: BillListComponent.getId()");
+  }
 }
