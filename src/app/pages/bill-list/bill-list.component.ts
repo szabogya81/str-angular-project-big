b35ccbb05c8d0ee874bbc9c43efaedb0cc46d96b
ethuringer <ethuringer@gmail.com>
2021-03-01 17:07:08 +0100

    bill-list amount

--- src/app/pages/bill-list/bill-list.component.ts
+++ src/app/pages/bill-list/bill-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Bill } from 'src/app/model/bill';
+import { Status } from 'src/app/model/status.enum';
 import { BillService } from 'src/app/services/bill.service';
 
 @Component({
@@ -14,7 +15,7 @@ export class BillListComponent implements OnInit {
   bills: Observable<Bill[]> = this.billService.getAll();
 
   filterText: string = '';
-  filterKey: string = 'Status';
+  filterKey: string = 'id';
   filterKeys: string[] = Object.keys(new Bill());
 
   constructor(private billService: BillService) { 
