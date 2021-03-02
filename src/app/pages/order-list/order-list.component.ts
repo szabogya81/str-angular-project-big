5c6557d9ee5aeb576f36d2101ce21c3e0c8c5302
Pablo-Genial <sporthold@yahoo.com>
2021-03-02 19:13:42 +0100

    Order delete confirm

--- src/app/pages/order-list/order-list.component.ts
+++ src/app/pages/order-list/order-list.component.ts
@@ -34,24 +34,23 @@ export class OrderListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+onUpdate(order: Order): void {
+    this.orderService.update(order).subscribe(
+    updatedOrder => console.log(updatedOrder)
+  )
+}
 
-  updateOrders() {
-    this.orderList = this.orderService.getAll();
-  }
-
-
-  onDelete(id: number) {
+  onDelete(id: number): void {
     this.orderService.remove(id).subscribe(
-      () => this.updateOrders()
+      () => console.log(`deleted id ${id}`)
     );
   }
 
-    onConfirmDelete(id: number) {
+  onConfirmDelete(id: number): void {
       this.confirmDialogService.confirmThis(
         "Are you sure to delete this Order?",
-        () => {
-          this.onDelete(id);
-        }, () => { })
+        () => { this.onDelete(id); },
+        () => { })
       }
 
   // sorter
