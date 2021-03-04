ff7c494d35793e8dee97173a0e5e8aad71cf9585
B.Viktor <benkovik@gmail.com>
2021-03-04 19:42:45 +0100

    new-bill updated

--- src/app/pages/new-bill/new-bill.component.ts
+++ src/app/pages/new-bill/new-bill.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { BillService } from 'src/app/services/bill.service';
 import { IdGeneratorService } from 'src/app/services/id-generator.service';
+import { OrderService } from 'src/app/services/order.service'
+import { Order } from 'src/app/model/order'
+import { Status } from 'src/app/model/status.enum';
+import { Observable } from 'rxjs';
 //import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -12,9 +16,11 @@ export class NewBillComponent implements OnInit {
 
   nextID: number;
   
+  orderList: Observable<Order[]> = this.orderService.getAll();
 
   constructor(
     private billService: BillService,
+    private orderService: OrderService,
     private idGenerator: IdGeneratorService
   ) {
     this.nextID = this.getId();
@@ -29,5 +35,12 @@ export class NewBillComponent implements OnInit {
 
   setId() {
     this.nextID = this.idGenerator.getNextUniqueID(this.billService);
+  //  this.getOrderIds();
   }
+
+  // getOrderIds() {
+  //   this.orderService.getAll().subscribe(
+  //     records => { this.orderIdList = records.filter(rec => rec.status==Status.New) }); //.map(rec => rec.id)
+  //     console.log(this.orderIdList);
+  // };
 }
